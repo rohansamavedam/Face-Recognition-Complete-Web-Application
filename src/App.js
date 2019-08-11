f2ecc0b9662eef6f7976bf4fa9c3e04a6bd6e51b
Rohan Samavedam <rohansamavedam@gmail.com>
2019-08-11 10:09:09 +0530

    frontendcomplete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import Signin from './components/Signin/Signin'
+import Register from './components/Register/Register'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Rank from './components/Rank/Rank'
 import './App.css';
 
@@ -24,38 +27,89 @@ const app = new Clarifai.App({
  });
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      signedin: false
     }
   }
 
-  onInputChange(event) {
-    console.log(event.target.value)
+  //Methods
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+    
   }
 
-  onButtonSubmit(){
-    console.log('click')
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
-    function(response) {
-      console.log(response)
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+
+  }
+
+
+  onInputChange = (event) => {                              //You need to use ES6 Syntax to access the 'this' keyowrd.
+    this.setState({input: event.target.value})
+
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})             //Setting a state in react is an asyncronous function, so u cannot access the item after setting state immideatly in that function. Hence the solution is using a callback function after setting the state, like this: setState(updater, callback)
+                                                            
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then((response) => {
+      this.displayFaceBox(this.calculateFaceLocation(response))
     },
     function(err) {
-      console.log(err)
+      alert('Error: There was a problem in detecting the Face')
     }
   );
+
   }
 
+  onRouteChange = (route) => {
+    if(route === 'home'){
+      this.setState({signedin: true})
+    }
+    else{
+      this.setState({signedin: false})
+    }
+
+    this.setState({ route: route })
+  }
+
+  //Render Main
+
   render() {
+    const { signedin, box, imageUrl, route } = this.state;
     return (
       <div className="App">
-        <Particles className='particles' params = { particlesOptions } />
-        <Navigation/>
-        <Logo/>
-        <Rank/>
-        <ImageLinkForm onInputChange = { this.onInputChange } onButtonSubmit = { this.onButtonSubmit } />
+        <Particles className='particles' params = { particlesOptions } />  
+        <Navigation onRouteChange={ this.onRouteChange } isSignedIn={signedin}/>
+         { route === 'home'
+            ? <div> 
+              <Logo/>
+              <Rank/>
+              <ImageLinkForm onInputChange = { this.onInputChange } onButtonSubmit = { this.onButtonSubmit } />
+              <FaceRecognition box = { box } imageUrl = { imageUrl } />
+            </div>
+            : (
+              route === 'signin'
+              ? <Signin onRouteChange={ this.onRouteChange }/>
+              : <Register onRouteChange={ this.onRouteChange }/>
+            )
+          }
       </div>
     );
   }
